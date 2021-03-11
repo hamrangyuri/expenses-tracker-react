ce41806e856a8e83e2f826f6b33e0a9eb29004c5
Gyuri <hamrangyuri@gmail.com>
2021-03-11 17:12:05 +0100

    Initial

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,50 @@
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+function AddTransaction() {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTransaction = {
+      id: Math.random() * 10000000,
+      text: text,
+      amount: parseInt(amount),
+    };
+    addTransaction(newTransaction);
+  };
+
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input
+            type="text"
+            placeholder="Enter text..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br />
+            (negative - expense, positive - income)
+          </label>
+          <input
+            type="number"
+            placeholder="Enter amount..."
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </>
+  );
+}
+
+export default AddTransaction;
